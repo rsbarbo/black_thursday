572783a58e4104dfa15c51a7b72b91e9f8bc169a
SusiIrwin <susi.irwin@gmail.com>
2016-07-26 16:49:02 -0600

    finished merchant and merchant test

---
+++ test/merchant_test.rb
@@ -0,0 +1,19 @@
+require "./test_helper"
+require "./lib/merchant"
+
+class MerchantTest < Minitest::Test
+    attr_reader :m
+
+  def setup
+    @m = Merchant.new({:id => 5, :name => "Turing School"})
+  end
+
+  def test_returns_id
+    assert_equal 5, m.id
+  end
+
+  def test_returns_name
+    assert_equal "turing school", m.name
+  end
+
+end
