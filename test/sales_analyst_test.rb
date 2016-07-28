8adab8ad82e541e7196a421cafd79b1302b236be
Raphael Barbo <rsbarbo@gmail.com>
2016-07-27 19:29:06 -0600

    in order to pass spec harness added some comments to item.rb file on unit_price, please refer to it, here is latest spec harness status for iteration 1 = 31 examples, 4 failures, iteration 0 all will pass if unit_price is divided by 100, so we will check once again with staff tomorrow. Added two tests to sales analyst, and both passes our rake run currently have status of 40 runs, 54 assertions, 0 failures, 0 errors, 2 skips, will continue with Super Susi tomorrow.

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -2,7 +2,19 @@ require_relative "../test_helper"
 require_relative "../lib/sales_analyst"
 
 class SalesAnalystTest < Minitest::Test
+  attr_reader :se, :sa
+  def setup
+    @se = SalesEngine.from_csv({:items => "./data/items.csv", :merchants => "./data/merchants.csv"})
+    @sa = SalesAnalyst.new(se)
+  end
 
   def test_it_can_return_average_items_per_merchant
+    assert_equal 2.88, sa.average_items_per_merchant
   end
+
+  def test_it_can_return_average_items_per_merchant_standard_deviation
+    assert_equal 3.26, sa.average_items_per_merchant_standard_deviation
+  end
+
+
 end
