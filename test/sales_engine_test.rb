0fca0d79d6ac6fa297eca5b42768ae5374791eb0
Raphael Barbo <rsbarbo@gmail.com>
2016-07-30 09:50:46 -0600

    added tests to sales_engine

--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -5,20 +5,20 @@ class SalesEngineTest < Minitest::Test
   attr_reader :input
 
   def setup
-  @input = {:items=>"./test/support/items_test.csv", :merchants=>"./test/support/merchants_test.csv", :invoices=>"./test/support/invoices_test.csv"}
+    @input = {:items=>"./test/support/items_test.csv", :merchants=>"./test/support/merchants_test.csv", :invoices=>"./test/support/invoices_test.csv"}
   end
 
-def test_sales_engine_has_access_to_item_repo_class
-  assert_equal(ItemRepo, SalesEngine.new(input).items.class)
-end
+  def test_sales_engine_has_access_to_item_repo_class
+    assert_equal(ItemRepo, SalesEngine.new(input).items.class)
+  end
 
-def test_sales_engine_has_access_to_merchant_repo_class
-  assert_equal(MerchantRepo, SalesEngine.new(input).merchants.class)
-end
+  def test_sales_engine_has_access_to_merchant_repo_class
+    assert_equal(MerchantRepo, SalesEngine.new(input).merchants.class)
+  end
 
-def test_sales_engine_has_access_to_invoice_repo_class
-  assert_equal(InvoiceRepo, SalesEngine.new(input).invoices.class)
-end
+  def test_sales_engine_has_access_to_invoice_repo_class
+    assert_equal(InvoiceRepo, SalesEngine.new(input).invoices.class)
+  end
 
 
 end
