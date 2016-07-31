a257beb8aa8c5ec0766afc1a7be9b314339d70a7
Raphael Barbo <rsbarbo@gmail.com>
2016-07-31 11:36:25 -0600

    added invoice_item and its tests, also half way through invoice_item_repo without tests yet, need to go back and fix that asap.

---
+++ lib/invoice_item_repo.rb
@@ -0,0 +1,23 @@
+require_relative "../lib/invoice_item"
+require "csv"
+
+class InvoiceItemRepo
+  attr_reader :engine
+  attr_accessor :all_invoice_items
+
+  def initialize(file_path, sales_engine)
+    @all_invoice_items = []
+    @engine = sales_engine
+    @load_csvs = load_csvs(file_path)
+  end
+
+  def load_csvs(file_path)
+    content = CSV.open(file_path, headers: true, header_converters: :symbol)
+    content.each do |row|
+      all_invoices << InvoiceItem.new(row, self)
+    end
+  end
+
+  def all
+    all_invoice_items
+  end
