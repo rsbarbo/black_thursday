cac2cfa0aa313df412aa73ad8aee5b8a9d9e782a
Raphael Barbo <rsbarbo@gmail.com>
2016-08-02 21:34:01 -0600

    finished refactoring sales analyst

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -53,27 +53,9 @@ class SalesAnalyst
     ((all_invoices.count)/(all_merchant.count.to_f)).round(2)
   end
 
-  def collct_of_invs_cnts
-    collection  = all_merchant.map do |merchant|
-      merchant.invoices.count
-    end
-    collection
-  end
-
-  def inv_qty
-    collct_of_invs_cnts.count
-  end
-
   def average_invoices_per_merchant_standard_deviation
     avg_inv_p_mrchnt = average_invoices_per_merchant
     invoice_deviation_calculator(collct_of_invs_cnts, avg_inv_p_mrchnt)
-  end
-
-  def invoice_deviation_calculator(collct_of_invs_cnts, average_invs_p_merchant)
-    pre_deviation = (collct_of_invs_cnts.reduce(0) do |accounter, average_num|
-      accounter + ((average_num - average_invs_p_merchant) ** 2)
-    end)/(inv_qty - 1).to_f
-    Math.sqrt(pre_deviation).round(2)
   end
 
   def top_merchants_by_invoice_count
@@ -90,23 +72,6 @@ class SalesAnalyst
     all_merchant.find_all do |merchant|
       merchant.invoices.count < lower_inv_count
     end
-  end
-
-  def formatting_inv_cnt_per_day
-    all_invoices.reduce(Hash.new(0)) do |result, invoices|
-      invoice_day = invoices.created_at.strftime("%A")
-      result[invoice_day] += 1
-      result
-    end
-  end
-
-  def top_day_deviation_calculator
-    @avg_inv_per_day = (all_invoices.count / 7).to_f
-    day_totals = formatting_inv_cnt_per_day.values
-    pre_deviation = (day_totals.reduce(0) do |sum, avg_num|
-      sum + ((avg_num - avg_inv_per_day) ** 2)
-    end)/(day_totals.count - 1).to_f
-    Math.sqrt(pre_deviation).round(2)
   end
 
   def top_days_by_invoice_count
@@ -127,26 +92,6 @@ class SalesAnalyst
     invoices = se.invoices.find_all_by_date(date)
     inv_ids = invoices.map {|invoice| invoice.id}
     finding_total_revenue_by_date(invoices, inv_ids)
-  end
-
-  def finding_total_revenue_by_date(invoices, inv_ids)
-    inv_items = se.invoice_items
-    all_inv_itms = inv_ids.map {|id| inv_items.
-      find_all_by_invoice_id(id)}.flatten
-    returning_revenue_date(inv_items, all_inv_itms)
-  end
-
-  def returning_revenue_date(inv_items, all_inv_itms)
-    result = all_inv_itms.map do |inv_itms|
-      inv_itms.quantity * inv_itms.unit_price
-    end
-    result.reduce(:+)
-  end
-
-  def ranking_merchants_by_revenue
-        all_merchant.sort_by do |merchant|
-        merchant.revenue
-    end.reverse
   end
 
   def top_revenue_earners(number = 20)
--- lib/sales_analyst_assist.rb
+++ lib/sales_analyst_assist.rb
@@ -39,4 +39,59 @@ module SalesAnalystAssistant
     Math.sqrt(pre_deviation).round(2)
   end
 
+  def formatting_inv_cnt_per_day
+    all_invoices.reduce(Hash.new(0)) do |result, invoices|
+      invoice_day = invoices.created_at.strftime("%A")
+      result[invoice_day] += 1
+      result
+    end
+  end
+
+  def collct_of_invs_cnts
+    collection  = all_merchant.map do |merchant|
+      merchant.invoices.count
+    end
+    collection
+  end
+
+  def inv_qty
+    collct_of_invs_cnts.count
+  end
+
+  def invoice_deviation_calculator(collct_of_invs_cnts, average_invs_p_merchant)
+    pre_deviation = (collct_of_invs_cnts.reduce(0) do |accounter, average_num|
+      accounter + ((average_num - average_invs_p_merchant) ** 2)
+    end)/(inv_qty - 1).to_f
+    Math.sqrt(pre_deviation).round(2)
+  end
+
+  def top_day_deviation_calculator
+    @avg_inv_per_day = (all_invoices.count / 7).to_f
+    day_totals = formatting_inv_cnt_per_day.values
+    pre_deviation = (day_totals.reduce(0) do |sum, avg_num|
+      sum + ((avg_num - avg_inv_per_day) ** 2)
+    end)/(day_totals.count - 1).to_f
+    Math.sqrt(pre_deviation).round(2)
+  end
+
+  def finding_total_revenue_by_date(invoices, inv_ids)
+    inv_items = se.invoice_items
+    all_inv_itms = inv_ids.map {|id| inv_items.
+      find_all_by_invoice_id(id)}.flatten
+    returning_revenue_date(inv_items, all_inv_itms)
+  end
+
+  def returning_revenue_date(inv_items, all_inv_itms)
+    result = all_inv_itms.map do |inv_itms|
+      inv_itms.quantity * inv_itms.unit_price
+    end
+    result.reduce(:+)
+  end
+
+  def ranking_merchants_by_revenue
+        all_merchant.sort_by do |merchant|
+        merchant.revenue
+    end.reverse
+  end
+
 end
